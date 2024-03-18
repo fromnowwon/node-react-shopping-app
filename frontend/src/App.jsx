de053eb8763ca60aae0cd30eec09cd6db2c78535
cw <fromnowwon@gmail.com>
2024-03-18 21:24:27 +0900

    setting router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function Layout() {
+	return (
+		<div className="flex flex-col h-screen justify-between">
+			<Navbar />
+			<main className="mb-auto w-10/12 max-w-4xl mx-auto">
+				<Outlet />
+			</main>
+			<Footer />
+		</div>
+	);
+}
 
 function App() {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello!</h1>
-		</>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<LandingPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
+			</Route>
+		</Routes>
 	);
 }
 
