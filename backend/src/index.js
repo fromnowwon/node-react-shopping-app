dc8097babe524d269618c4dc14402780ce7008a5
cw <fromnowwon@gmail.com>
2024-03-19 11:03:39 +0900

    정적 파일 제공

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path"); // 절대 경로 사용 모듈
 
 const app = express();
 const port = 4000;
@@ -6,6 +7,9 @@ const port = 4000;
 app.get("/", (req, res) => {
 	res.send("hello!!");
 });
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, "../uploads")));
 
 app.listen(port, () => {
 	console.log(`${port}번에서 실행됨`);
