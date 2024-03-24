ffa89ad274ce3cca5a84ee18e2ed75e1685afe3d
cw <fromnowwon@gmail.com>
2024-03-24 14:33:29 +0900

    fix: props 타입 명시

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,7 +1,12 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const ProtectedRoutes = ({ isAuth }) => {
 	return isAuth ? <Outlet /> : <Navigate to={"/login"} />;
+};
+
+ProtectedRoutes.propTypes = {
+	isAuth: PropTypes.bool.isRequired,
 };
 
 export default ProtectedRoutes;
