e59bb0e5d3a65a82df5dc1a8a1565dea8a72b52f
cw <fromnowwon@gmail.com>
2024-03-30 20:44:57 +0900

    feat: 파일 업로드 후 db에 저장되도록 함

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -2,9 +2,30 @@ import PropTypes from "prop-types";
 import Dropzone from "react-dropzone";
 
 const FileUpload = ({ onImageChange, images }) => {
+	const handleDrop = async (files) => {
+		let formData = new FormData();
+
+		const config = {
+			header: { "content-type": "multipart/form-data" },
+		};
+
+		formData.append("file", files[0]);
+
+		try {
+			const response = await axiosInstance.post(
+				"/products/image",
+				formData,
+				config
+			);
+			onImageChange([...images, response.data.fileName]);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<div className="flex gap-4">
-			<Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+			<Dropzone onDrop={handleDrop}>
 				{({ getRootProps, getInputProps }) => (
 					<section className="min-w-[300px] h-[300px] border flex items-center justify-center">
 						<div {...getRootProps()}>
