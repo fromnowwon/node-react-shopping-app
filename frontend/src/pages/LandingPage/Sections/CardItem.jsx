c53df8b1099e336b96ce223912e31ef09b0852a5
cw <fromnowwon@gmail.com>
2024-04-08 16:16:04 +0900

    feat: 상품 이미지 슬라이더 적용

--- frontend/src/pages/LandingPage/Sections/CardItem.jsx
+++ frontend/src/pages/LandingPage/Sections/CardItem.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import ImageSlider from "../../../components/ImageSlider";
 
 const CardItem = ({ product }) => {
 	return (
 		<div className="border-[1px] border-gray-300">
+			<ImageSlider images={product.images} />
 			<Link to={`/product/${product._id}`}>
 				<p className="p-1">{product.title}</p>
 				<p className="p-1">{product.continents}</p>
